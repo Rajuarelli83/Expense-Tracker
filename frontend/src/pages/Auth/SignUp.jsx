73604f479e412f4f34acce972d998bb8f5550a85
Raju Arelli <158052899+Rajuarelli83@users.noreply.github.com>
2025-08-21 10:28:41 +0530

    changing overlapping SignUp.jsx

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -82,8 +82,9 @@ const SignUp = () => {
     return (
         <AuthLayout >
         <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-            <h3 className="text-xl font-semobold text-black text-center">Create an Account</h3>
-            <p className="text-xs text-slate-700 mt-[5px] mb-6 text-center">Track your financial data expertly.</p>
+            <h3 className=" hidden md:block text-xl font-semobold text-black text-center">Create an Account</h3>
+            <p className=" hidden md:block text-xs text-slate-700 mt-[5px] mb-6 text-center">Track your financial data expertly.</p>
+           <p className=" block md:hidden text-xs text-slate-700 mt-[5px] mb-6 text-center">Register Now.</p>
            
            <form onSubmit={handleSignUp} >
             
